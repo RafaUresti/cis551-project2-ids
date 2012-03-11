c33ad4726e62e6299a681cb924b7c239fc995933
bbreck <bbreck@19423587-b744-fe99-c7a5-528f5bd0fea1>
2012-03-11 21:41:54 +0000

    Testing UDP.

--- trunk/src/ProtocolRuleProcessor.java
+++ trunk/src/ProtocolRuleProcessor.java
@@ -1,18 +1,90 @@
+import java.util.ArrayList;
 import java.util.List;
 
 import net.sourceforge.jpcap.net.Packet;
+import net.sourceforge.jpcap.net.UDPPacket;
 public class ProtocolRuleProcessor
 {
-	private List<Rule> rules;
-
+	private List<Rule> udpRules;
+	private List<Rule> tcpRules;
+	private String host;
+	
 	public ProtocolRuleProcessor(List<Rule> rules)
 	{
-		this.rules = rules;
+		if (rules.size() > 0)
+		{
+			host = rules.get(0).getHost();
+		}
+		
+		cacheRules(rules);
 
 	}
 
+	
 	public void processRules(Packet packet)
 	{
-		System.out.println("Protocol Processing: "+packet.toColoredString(true));
+		if (packet instanceof UDPPacket)
+		{
+			processUDP((UDPPacket)packet);
+		}
+		
+	}
+	
+	private void processUDP(UDPPacket packet)
+	{
+		boolean isReceive = packet.getDestinationAddress().equals(host);
+		
+		for (Rule rule : udpRules)
+		{
+			ProtocolRule prule = rule.getPrule();
+			if (!prule.getSrcPort().equals("any") &&
+			    packet.getSourcePort() != Integer.parseInt(rule.getPrule().getSrcPort()))
+			{
+				continue;
+			}
+			if (!prule.getDstPort().equals("any") &&
+				packet.getDestinationPort() != Integer.parseInt(prule.getDstPort()))
+			{
+				continue;
+			}
+			
+			if (!prule.getIp().equals("any") &&
+				((isReceive && !packet.getSourceAddress().equals(prule.getIp())) ||
+				 (!isReceive && !packet.getDestinationAddress().equals(prule.getIp()))))
+			{
+				continue;
+			}
+			
+			String data = new String(packet.getData());
+			for (SubRule srule : prule.getSubRule())
+			{
+				if ((isReceive && data.contains(srule.getRecv())) ||
+					(!isReceive && data.contains(srule.getSnd())))
+				{
+					System.out.println("Rule: "+rule.getName());
+				}
+			}
+		}
+	}
+	
+	private void cacheRules(List<Rule> rules)
+	{
+		udpRules = new ArrayList<Rule>();
+		tcpRules = new ArrayList<Rule>();
+		for (Rule rule : rules)
+		{
+			if (rule.getPrule() != null)
+			{
+				ProtocolRule pr = rule.getPrule();
+				if (pr.getProtocol().equals("udp"))
+				{
+					udpRules.add(rule);
+				}
+				else
+				{
+					tcpRules.add(rule);
+				}
+			}
+		}
 	}
 }
